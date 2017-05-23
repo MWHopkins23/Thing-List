06179fe478247f4c84e9b0a6c2c115cc82c259ee
Matthew Jennifer <MatthewHopkins@matthews-air.coldspring.marian.edu>
2017-05-23 12:20:24 -0400

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+      
+      <div>
+        <h1>Thing List</h1>
+        <h2> So Many Things </h2>
+      </div>
+
+    )
+}
+
+export default Header
