284cc6fd56c2089a611571de7412699c286f03f5
MWHopkins23 <mwhopkin19@wabash.edu>
2017-05-25 09:53:03 -0400

    Sync with Firebase

--- src/AddThingButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './AddThingButton.css'
-
-const AddThingButton = ({ addThing }) => {
-  return <button className="AddThingButton" onClick={addThing}>Add Thing</button>
-}
-
-export default AddThingButton
